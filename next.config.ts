0b3ed694796856258920e3b509c10473262d9aa4
YanirMidler24 <yanir.midler@gmail.com>
2025-04-30 17:11:21 +0300

    unoptimized: true, // ✅ חובה עבור Netlify

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // ⬇️ תמונות מתוך public נתמכות כברירת מחדל
-    formats: ["image/webp", "image/avif"],
-    deviceSizes: [640, 768, 1024, 1280, 1600, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    // אין צורך להוסיף domains כי אתה לא טוען תמונות חיצוניות
+    unoptimized: true, // ✅ חובה עבור Netlify
   },
 };
 
